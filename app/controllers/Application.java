08772188eff258967b4f00f41fd5d695ca80a69b
Lowell Buttorff <lbuttorff@gmail.com>
2018-03-22 16:10:48 -0400

    Add CSRF tokens Make search a post form

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,12 +1,13 @@
 package controllers;
 
-import play.*;
+import play.filters.csrf.AddCSRFToken;
 import play.mvc.*;
 
 import views.html.*;
 
 public class Application extends Controller {
 
+    @AddCSRFToken
     public Result index() {
         return ok(main.render());
     }
