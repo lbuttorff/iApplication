a433d065d588533a8f968db766dc0e308d081cab
Lowell Buttorff <lbuttorff@gmail.com>
2018-04-06 10:49:05 -0400

    Add services to User model Change Mentor features to be enumerated Comment User class

--- app/models/User.java
+++ app/models/User.java
@@ -5,10 +5,14 @@ import org.mindrot.jbcrypt.BCrypt;
 import play.data.format.Formats;
 import play.data.validation.Constraints;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
+/**
+ * User model class that extends the Ebean Model class for interaction directly with the database
+ * Created by Lowell Buttorff - Updated 20180406
+ */
 @Entity
 public class User extends Model {
     @Id
@@ -16,6 +20,7 @@ public class User extends Model {
     private String firstName;
     private String lastName;
     @Constraints.Required
+    @Column(unique = true)
     private String email;
     @Constraints.Required
     private String passwordHash;
@@ -23,16 +28,23 @@ public class User extends Model {
     @Formats.DateTime(pattern="dd/MM/yyyy")
     private Date joinDate;
     private int type;
-    private String campus;
-    private String standing;
-    private String department;
-    //private String scores;
+    private int campus;
+    private int standing;
+    private int department;
+    private List<Integer> services;
+    //private String scores; //Still not sure how we want to implement this 20180406
 
-    //TODO: add services to the User model
-
-    //Static finder that will return a user based on the extend methods of Model
+    /**
+     * Finder method that takes advantage of the Ebean Model class features in order to access data in the database
+     */
     public static final Finder<Long, User> find = new Finder<>(User.class);
 
+    /**
+     * Checks if a User exists and if the password given matches the one in the database
+     * @param email The String email address of a given User
+     * @param pass The String password of a given User
+     * @return null if the User does not exist or has the wrong password. A User object if authenticated
+     */
     public static User authenticate(String email, String pass){
         User user = findByEmail(email);
         if(user != null && BCrypt.checkpw(pass, user.getPasswordHash())){
@@ -42,10 +54,24 @@ public class User extends Model {
         }
     }
 
+    /**
+     * Attempts to find a User in the database using the Finder method and by filtering the results by email
+     * @param email The String email address of a given User
+     * @return A User object from the database or null if no user exists
+     */
     public static User findByEmail(String email){
         return find.query().where().eq("email", email).findUnique();
     }
 
+    /**
+     * Constructor used for building a default User
+     * @param fName First name as a String
+     * @param lName Last name as a String
+     * @param email Email address as a String
+     * @param pass Password as a String
+     * @param t Type as an int
+     * @param age Age as an int
+     */
     public User(String fName, String lName, String email, String pass, int t, int age){
         this.firstName = fName;
         this.lastName = lName;
@@ -54,13 +80,36 @@ public class User extends Model {
         this.age = age;
         this.joinDate = new Date();
         this.type = t;
-        //TODO: Implement these other features of user
-        //this.campus = campus;
-        //this.standing = standing;
-        //this.department = department;
-        //this.scores = scores;
     }
 
+    /**
+     * Constructor used for building a User with Mentor features
+     * @param fName First name as a String
+     * @param lName Last name as a String
+     * @param email Email address as a String
+     * @param pass Password as a String
+     * @param t Type as an int
+     * @param age Age as an int
+     * @param c Campus as an int
+     * @param s Standing as an int
+     * @param d Department as an int
+     * @param services Services provided as a List of Integers
+     */
+    public User(String fName, String lName, String email, String pass, int t, int age, int c, int s, int d, List<Integer> services){
+        this.firstName = fName;
+        this.lastName = lName;
+        this.email = email;
+        this.passwordHash = BCrypt.hashpw(pass, BCrypt.gensalt());
+        this.age = age;
+        this.joinDate = new Date();
+        this.type = t;
+        this.campus = c;
+        this.standing = s;
+        this.department = d;
+        this.services = services;
+    }
+
+    //Standard Getters and Setters
     public long getId() {
         return id;
     }
@@ -120,32 +169,36 @@ public class User extends Model {
     public String getPasswordHash() {
         return passwordHash;
     }
-/*
-    public void setPasswordHash(String passwordHash) {
-        this.passwordHash = passwordHash;
-    }
-*/
-    public String getCampus() {
+
+    public int getCampus() {
         return campus;
     }
 
-    public void setCampus(String campus) {
+    public void setCampus(int campus) {
         this.campus = campus;
     }
 
-    public String getStanding() {
+    public int getStanding() {
         return standing;
     }
 
-    public void setStanding(String standing) {
+    public void setStanding(int standing) {
         this.standing = standing;
     }
 
-    public String getDepartment() {
+    public int getDepartment() {
         return department;
     }
 
-    public void setDepartment(String department) {
+    public void setDepartment(int department) {
         this.department = department;
+    }
+
+    public List<Integer> getServices() {
+        return services;
+    }
+
+    public void setServices(List<Integer> services) {
+        this.services = services;
     }
 }
